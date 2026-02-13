3d24704387d24465675124ef49d4138f2e77c1d8
Cristina Vacas Lopez <cristinavltrabajo@gmail.com>
2026-02-13 21:54:31 +0100

    margenes del layout

--- frontend/components/layout/AppLayout.tsx
+++ frontend/components/layout/AppLayout.tsx
@@ -16,7 +16,7 @@ export function AppLayout({title, subtitle, children}: AppLayoutProps) {
   return (
     <div className="flex min-h-screen h-screen">
       <Sidebar className="h-full"/>
-      <div className="flex-1 h-full overflow-y-auto lg:p-10">
+      <div className="flex-1 h-full overflow-y-auto">
         <PageHeader title={title} subtitle={subtitle} />
         <main className="flex-1 p-6 pt-4 md:pt-6 lg:px-10">
           {children}
