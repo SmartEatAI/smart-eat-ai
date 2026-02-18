585b3eca36fe6f57bb9d11f96bff33159a03be47
Elias Robles <149778129+erobrui2812@users.noreply.github.com>
2026-02-18 14:02:49 +0100

    Modelo Plan diagrama er + cambio de activo

---
+++ backend/app/models/plan.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, ForeignKey, TIMESTAMP, Boolean
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Plan(Base):
+    __tablename__ = "plan"
+    id = Column(Integer, primary_key=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+    fecha_creacion = Column(TIMESTAMP)
+    fecha_modificacion = Column(TIMESTAMP)
+    activo = Column(Boolean, default=True)
+
+    usuario = relationship("Usuario", back_populates="planes")
+    menus_diarios = relationship("MenuDiario", back_populates="plan")
