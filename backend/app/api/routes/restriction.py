c86ea3a6e855e7ef8e1835808afb193924cf7d5e
Cristina Vacas Lopez <cristinavltrabajo@gmail.com>
2026-02-20 17:43:42 +0100

    Actualizacion de dependencias del esquema de restriction

--- backend/app/api/routes/restriction.py
+++ backend/app/api/routes/restriction.py
@@ -1,16 +1,16 @@
+from backend.app.schemas.category import CategoryResponse
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas.restriction import  RestrictionBase, RestrictionResponse
 from app.crud import restriction as crud
 from app.models import User
 from app.api.deps import get_current_user
 
 router = APIRouter(prefix="/restriction", tags=["Restriction"])
 
-@router.post("/", response_model=RestrictionResponse)
+@router.post("/", response_model=CategoryResponse)
 def create_restriction(
-    restriction_in: RestrictionBase, 
+    restriction_in: CategoryResponse, 
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user) 
 ):
