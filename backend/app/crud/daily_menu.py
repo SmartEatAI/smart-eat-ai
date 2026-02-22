1d2f71aafc6ebba22153cda9f1fb92894f0afdea
Cristina Vacas Lopez <cristinavltrabajo@gmail.com>
2026-02-22 01:10:09 +0100

    Correccion de los endpoints de daily menu, testeado y funcional

--- backend/app/crud/daily_menu.py
+++ backend/app/crud/daily_menu.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from app.models.meal_detail import MealDetail
-from app.schemas.meal_detail import MealDetailBase
+from app.models.daily_menu import DailyMenu
+from app.schemas.daily_menu import DailyMenuBase
 
 def get_daily_menu_by_id(db: Session, daily_menu_id: int):
   """Obtiene un menú diario por su ID."""
   try:
-    return db.query(MealDetail).filter(MealDetail.daily_menu_id == daily_menu_id).all()
+    return db.query(DailyMenu).filter(DailyMenu.id == daily_menu_id).first()
   except Exception as e:
     print(f"Error al obtener menú diario por ID: {str(e)}")
 
@@ -15,26 +15,14 @@ def get_daily_menu_by_id(db: Session, daily_menu_id: int):
       detail="Error interno al consultar el menú diario en la base de datos"
     )
 
-def get_daily_menu_by_plan_id(db: Session, plan_id: int):
-  """Obtiene un menú diario por su ID de plan."""
-  try:
-    return db.query(MealDetail).filter(MealDetail.plan_id == plan_id).all()
-  except Exception as e:
-    print(f"Error al obtener menú diario por ID de plan: {str(e)}")
-
-    raise HTTPException(
-      status_code=500, 
-      detail="Error interno al consultar el menú diario en la base de datos"
-    )
-
-def create_daily_menu(db: Session, obj_in: MealDetailBase):
+def create_daily_menu(db: Session, obj_in: DailyMenuBase):
   """Crea un nuevo menú diario."""
-  db_meal_detail = MealDetail(**obj_in.model_dump())
+  db_daily_menu = DailyMenu(**obj_in.model_dump())
   try:
-    db.add(db_meal_detail)
+    db.add(db_daily_menu)
     db.commit()
-    db.refresh(db_meal_detail)
-    return db_meal_detail
+    db.refresh(db_daily_menu)
+    return db_daily_menu
   except Exception as e:
     print(f"Error al crear menú diario: {str(e)}")
 
