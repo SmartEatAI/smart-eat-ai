6f6f256a4da0f83e3cb0e572d124a8adaf21fbd0
rxy94 <ruyi1994@gmail.com>
2026-02-23 14:16:56 +0100

    eliminar import EatingStyleService en ruta diet_type

--- backend/app/api/routes/diet_type.py
+++ backend/app/api/routes/diet_type.py
@@ -2,7 +2,6 @@ from app.schemas.category import CategoryBase
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.services.eating_style import EatingStyleService
 from app.services.diet_type import DietTypeService
 
 router = APIRouter(prefix="/diet_type")
