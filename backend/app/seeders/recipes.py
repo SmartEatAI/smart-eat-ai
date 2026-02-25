de720f99775fee94fe5b59d2b56b4559ffa84ab6
rxy94 <ruyi1994@gmail.com>
2026-02-25 15:30:15 +0100

    cambiar a ruta absoluta para cargar recipes.json

--- backend/app/seeders/recipes.py
+++ backend/app/seeders/recipes.py
@@ -1,3 +1,4 @@
+import os
 import json
 from sqlalchemy.orm import Session
 from app.crud.recipe import create_recipe
@@ -5,8 +6,11 @@ from app.schemas.recipe import RecipeCreate
 
 
 def seed_recipes(db: Session):
+    # Ruta absoluta basada en la ubicación de este archivo
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    recipes_path = os.path.join(current_dir, "..", "data", "recipes.json")
 
-    with open("app/data/recipes.json", encoding="utf-8") as f:
+    with open(recipes_path, encoding="utf-8") as f:
         recipes_data = json.load(f)
 
     for recipe_data in recipes_data:
